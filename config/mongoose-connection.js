2f749fd04ecee118b60a2e41fcfa94f4b3edccfa
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-30 16:33:40 +0530

    mongoose connection config

---
+++ config/mongoose-connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const config = require("config")
+const debug = require("debug")('development:mongoose');
+
+mongoose
+    .connect(`${config.get("MONGODB_URI")}/pinterest`)
+    .then(function () {
+        debug('database connected !');
+    })
+    .catch(function (err) {
+        console.log('connection error', err);
+    })
+
+module.exports = mongoose.connection;
